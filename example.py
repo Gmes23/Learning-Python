d5623a98782dfffcc519dfa614aa5069105fe334
Gmes23 <gmesda23@gmail.com>
2017-01-29 23:27:54 -0500

    basic python commands

---
+++ example.py
@@ -0,0 +1,27 @@
+colors = ['red', 'blue', 'yellow']
+
+print(sorted(colors))
+
+lencolors = len(colors)
+
+print(lencolors)
+
+for value in range(1,6):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
+
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+minsquare = min(squares)
+print(minsquare)
+maxsquare = max(squares)
+print(maxsquare)
+
+print(525/65)
